16fa751f59e2d206072af1d23af2c2e9a174d4da
Carlin Hefner <carlinohefner@gmail.com>
2024-08-02 11:36:57 -0600

    try acc cancel silent value

--- selfdrive/car/tesla/carcontroller.py
+++ selfdrive/car/tesla/carcontroller.py
@@ -49,9 +49,9 @@ class CarController(CarControllerBase):
     #   counter = CS.das_control["DAS_controlCounter"]
     #   can_sends.append(self.tesla_can.create_longitudinal_commands(acc_state, target_speed, min_accel, max_accel, counter))
 
-      # Try disabling long (op or stock) with lat active (acc-state 0 = acc cancel generic, aka cruise disabled)
+      # Try disabling long (op or stock) with lat active (acc-state 13 = acc cancel generic silent, aka cruise standby)
       counter = CS.das_control["DAS_controlCounter"]
-      can_sends.append(self.tesla_can.create_longitudinal_commands(0, 0, 0, 0, counter))
+      can_sends.append(self.tesla_can.create_longitudinal_commands(13, 0, 0, 0, counter))
 
     # Cancel on user steering override, since there is no steering torque blending
     if hands_on_fault:
