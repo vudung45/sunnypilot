423082e27b0edf314b9300c145ebe0b6b56320d2
Carlin Hefner <carlinohefner@gmail.com>
2024-08-08 19:09:41 -0600

    Try 3-finger tap for mads

--- selfdrive/car/tesla/carstate.py
+++ selfdrive/car/tesla/carstate.py
@@ -150,6 +150,7 @@ class CarState(CarStateBase):
       ("SCCM_steeringAngleSensor", 100),
       ("DAS_bodyControls", 2),
       ("ID3F5VCFRONT_lighting", 10),
+      ("UI_status2", 100),
     ]
 
     return CANParser(DBC[CP.carFingerprint]["pt"], messages, CANBUS.vehicle)
